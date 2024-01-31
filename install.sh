ff17c692f1caa36809e7167379841ad3f4ae02b7
spilin <lyoshakr@gmail.com>
2024-01-31 19:09:28 +0200

    Cleanup

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 required_vars="NAME RPC_URL CHAIN_ID SILO_GENESIS"
 for var in $required_vars; do
   # If any required variable is not set, print an error message and exit
-  if [ -z "${!var}" ]; then
+  if [ -z "$(eval echo \$$var)" ]; then
     echo "Error: Environment variable $var is not set." >&2
     exit 1
   fi
@@ -34,11 +34,8 @@ wallet_connect_project_id=$NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
 dockercompose_template_file="./config/docker-compose-template.yaml"
 dockercompose_file="docker-compose.yaml"
 
-# Copy the Docker Compose template to the working directory
-cp "${dockercompose_template_file}" "${dockercompose_file}"
-
 # Replace placeholders in the Docker Compose file with actual values
-sed -i '' \
+sed \
     -e "s/{name}/$name/g" \
     -e "s/{rpc_url}/$rpc_url/g" \
     -e "s/{chain_id}/$chain_id/g" \
@@ -47,7 +44,7 @@ sed -i '' \
     -e "s/{favicon_generator_api_key}/$favicon_generator_api_key/g" \
     -e "s/{wallet_connect_project_id}/$wallet_connect_project_id/g" \
     -e "s/{explorer_url}/$explorer_url/g" \
-    $dockercompose_file
+    $dockercompose_template_file > $dockercompose_file
 
 # Define the paths for the proxy configuration template and the actual file
 proxy_template_file="./config/proxy/default.conf.template"
@@ -56,10 +53,7 @@ proxy_file="./data/proxy/default.conf.template"
 # Create the directory for the proxy configuration file if it doesn't exist
 mkdir -p "$(dirname "$proxy_file")"
 
-# Copy the proxy configuration template to the working directory
-cp "$proxy_template_file" "$proxy_file"
-
 # Replace placeholder in the proxy configuration file with actual value
-sed -i '' \
+sed \
     -e "s/{explorer_url}/$explorer_url/g" \
-    $proxy_file
+    $proxy_template_file > $proxy_file
