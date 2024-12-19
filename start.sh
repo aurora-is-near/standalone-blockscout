c5938dbb073e5c6b38f9b9562396757552de91d2
spilin <lyoshakr@gmail.com>
2024-12-20 00:36:41 +0200

    Add optional sidecar support

--- start.sh
+++ start.sh
@@ -1,24 +1,3 @@
 #!/bin/sh
 
-docker_pull_policy=""
-build_flag=false
-
-for arg in "$@"
-do
-    case $arg in
-        --pull)
-        docker_pull_policy="--pull always"
-        ;;
-        --build)
-        build_flag=true
-        ;;
-    esac
-done
-
-# Check if --build flag was passed and run build.sh if true
-if [ "$build_flag" = true ]; then
-    ./build.sh
-fi
-
-# Start Docker Compose with conditional pull policy
-docker compose -f docker-compose.yaml up -d $docker_pull_policy
+docker compose -f docker-compose.yaml up -d
